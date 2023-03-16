7b6fefa9f79f5f16bb16b4d554a50a7f9d1b366b
Adam Alidibirov <85851465+markarenin@users.noreply.github.com>
2023-03-16 14:32:14 +0200

    Add files via upload

---
+++ test_triangle.py
@@ -0,0 +1,22 @@
+import unittest
+from triangle import classify_triangle
+
+class TestTriangle(unittest.TestCase):
+  
+  def test_equilateral_triangle(self):
+    self.assertEqual(classify_triangle(1, 1, 1), "Equilateral")
+    self.assertEqual(classify_triangle(200, 200, 200), "Equilateral")
+  
+  def test_isosceles_triangle(self):
+    # One pair of sides is equal
+    self.assertEqual(classify_triangle(2, 2, 3), "Isosceles")
+    self.assertEqual(classify_triangle(199, 199, 198), "Isosceles")
+  
+  def test_scalene_triangle(self):
+    self.assertEqual(classify_triangle(2, 3, 4), "Scalene")
+    self.assertEqual(classify_triangle(97, 98, 99), "Scalene")
+    
+  def test_not_a_triangle(self):
+    self.assertEqual(classify_triangle(1, 1, 2), "NotATriangle")
+    self.assertEqual(classify_triangle(100, 100, 200), "NotATriangle")
+    
